8daf55e1cba82744562195ab1d310c6af2eeb6b6
Vlad Eminovici <veminovici@hotmail.com>
2023-05-12 12:54:30 +0300

    Add create_node to the kernel

--- examples/echo/main.rs
+++ examples/echo/main.rs
@@ -3,9 +3,11 @@
 use mael::{
     egress::{self, Egress, StdEgress},
     ingress,
+    kernel::{Kernel, StdKernel},
     message::Message,
+    node::Node,
     payload::{EgressInitExt, IngressInitExt},
-    pld, kernel::StdKernel,
+    pld,
 };
 
 pld!(
@@ -14,6 +16,27 @@ pld!(
         EchoOk { echo: String },
     }
 );
+
+struct MyNode {
+    node_id: String,
+    node_ids: Vec<String>,
+}
+
+impl Node for MyNode {
+    type Payload = MyEcho;
+
+    fn from_init(node_id: String, node_ids: Vec<String>) -> Self {
+        MyNode { node_id, node_ids }
+    }
+
+    fn handle_message<E: Egress>(
+        &mut self,
+        msg: Message<Self::Payload>,
+        egress: &E,
+    ) -> anyhow::Result<()> {
+        todo!()
+    }
+}
 
 fn read_echo(line: String) -> anyhow::Result<Message<MyEcho>> {
     let msg = serde_json::from_str::<Message<MyEcho>>(&line).unwrap();
@@ -44,11 +67,10 @@ fn reply_echo(egress: &StdEgress, msg: Message<MyEcho>) -> anyhow::Result<()> {
 fn main() {
     eprintln!("Starting Echo ...");
 
-    let kernel = StdKernel::spawn();
+    let kernel = StdKernel::<MyNode>::spawn();
 
-    // Init
-    let msg = kernel.ingress.read_init_msg().unwrap();
-    kernel.egress.reply_init_msg(msg).unwrap();
+    // Create the node
+    let node = kernel.create_node().unwrap();
 
     for line in &kernel.ingress {
         let msg = read_echo(line).unwrap();
--- src/kernel.rs
+++ src/kernel.rs
@@ -1,20 +1,56 @@
-use crate::{ingress::StdIngress, egress::StdEgress};
+use std::marker::PhantomData;
 
-pub trait Kernel {}
+use anyhow::bail;
 
-pub struct StdKernel {
-    pub ingress: StdIngress,
-    pub egress: StdEgress,
+use crate::{
+    egress::StdEgress,
+    ingress::StdIngress,
+    node::Node,
+    payload::{EgressInitExt, IngressInitExt},
+};
+
+pub trait Kernel<N>
+where
+    N: Node,
+{
+    fn create_node(&self) -> anyhow::Result<N>;
 }
 
-impl StdKernel {
+pub struct StdKernel<N> {
+    pub ingress: StdIngress,
+    pub egress: StdEgress,
+    phantom: PhantomData<N>,
+}
+
+impl<N> StdKernel<N>
+where
+    N: Node,
+{
     pub fn spawn() -> Self {
         let ingress = StdIngress::spawn();
         let egress = StdEgress::spawn();
 
-        StdKernel { ingress, egress }
+        StdKernel {
+            ingress,
+            egress,
+            phantom: PhantomData,
+        }
     }
 }
 
-impl Kernel for StdKernel {}
-
+impl<N> Kernel<N> for StdKernel<N>
+where
+    N: Node,
+{
+    fn create_node(&self) -> anyhow::Result<N> {
+        let msg = self.ingress.read_init_msg().unwrap();
+        match &msg.body.payload {
+            crate::payload::Init::Init { node_id, node_ids } => {
+                let node = N::from_init(node_id.clone(), node_ids.clone());
+                self.egress.reply_init_msg(msg)?;
+                Ok(node)
+            }
+            crate::payload::Init::InitOk => bail!("We are expecting an INIT message"),
+        }
+    }
+}
